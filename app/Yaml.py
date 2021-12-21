37783bf2e55a3a17690286298d59df7c73beb88a
dearxuan <1320699768@qq.com>
2021-12-21 13:23:40 +0800

    更新

--- app/Yaml.py
+++ app/Yaml.py
@@ -5,8 +5,15 @@ import os
 path_prefix = './'  # comment this line in deployment
 
 
-filepath = path_prefix + 'static/config.yml'
-f = open(filepath, 'r', encoding='utf-8')
+ymlPath = path_prefix + 'static/config.yml'
+partialPath = path_prefix + 'layout/partial/'
+f = open(ymlPath, 'r', encoding='utf-8')
 cont = f.read()
 
 yml = YAML.load(cont, Loader=YAML.FullLoader)
+
+with open(partialPath + 'header.html', 'r', encoding='utf-8') as f:
+    yml['header'] = f.read()
+
+with open(partialPath + 'footer.html', 'r', encoding='utf-8') as f:
+    yml['footer'] = f.read()

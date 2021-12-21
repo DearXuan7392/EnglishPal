16dcdbcb83162c6bbd0e1dda6e7031cf518a9c2c
dearxuan <1320699768@qq.com>
2021-12-21 10:05:27 +0800

    更新

--- app/main.py
+++ app/main.py
@@ -213,14 +213,16 @@ def userpage(username):
         page = '<meta charset="UTF8">\n'
         page += '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0, user-scalable=yes" />\n'
         page += '<meta name="format-detection" content="telephone=no" />\n'  # forbid treating numbers as cell numbers in smart phones
-        page += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'
-        page += '''
+        page += '<link href="static/res/bootstrap.css" rel="stylesheet">'
+        page += \
+'''
 <head>
-	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dearxuan7392/picture/aplayercss.css">
-        <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
-	    <script src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>
-	</head>
-        '''
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dearxuan7392/picture/aplayercss.css">
+    <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js"></script>
+</head>
+
+'''
         page += '<title>EnglishPal Study Room for %s</title>' % username
         page += '<div class="container-fluid">'
         page += '''

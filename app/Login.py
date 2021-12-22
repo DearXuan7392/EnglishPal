9ddf9c271bd936415e94d21a9f0f4c1be3f39745
dearxuan <1320699768@qq.com>
2021-12-22 18:46:41 +0800

    更新

--- app/Login.py
+++ app/Login.py
@@ -9,7 +9,7 @@ path_prefix = './'  # comment this line in deployment
 
 def verify_user(username, password):
     rq = RecordQuery(path_prefix + 'static/wordfreqapp.db')
-    password = mod5(username + password)
+    password = md5(username + password)
     rq.instructions_with_parameters("SELECT * FROM user WHERE name=? AND password=?", (username, password))
     rq.do_with_parameters()
     result = rq.get_results()
@@ -19,7 +19,7 @@ def verify_user(username, password):
 def add_user(username, password):
     start_date = datetime.now().strftime('%Y%m%d')
     expiry_date = '20211230'
-    password = mod5(username + password)
+    password = md5(username + password)
     rq = InsertQuery(path_prefix + 'static/wordfreqapp.db')
     rq.instructions("INSERT INTO user Values ('%s', '%s', '%s', '%s')" % (username, password, start_date, expiry_date))
     rq.do()
@@ -36,7 +36,7 @@ def check_username_availability(username):
 def change_password(username, old_psd, new_psd):
     if not verify_user(username, old_psd):  # 旧密码错误
         return False
-    password = mod5(username + new_psd)
+    password = md5(username + new_psd)
     rq = InsertQuery(path_prefix + 'static/wordfreqapp.db')
     rq.instructions("UPDATE user SET password = '%s' WHERE name = '%s'" % (password, username))
     rq.do()
@@ -54,7 +54,7 @@ def get_expiry_date(username):
         return '20191024'
 
 
-def mod5(str):
+def md5(str):
     return str
     h = hashlib.md5(str.encode(encoding='utf-8'))
     return h.hexdigest()
